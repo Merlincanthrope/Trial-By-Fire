e87253bd8913d6971cdb3af3ae57e20ebd39eec0
Merlin <148911202+Merlincanthrope@users.noreply.github.com>
2024-08-20 17:11:09 +0000

    yes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1080
-canvas.height = 920
+canvas.width = 1080;
+canvas.height = 920;
 
+const floorY = 700;
+
+class Player {
+    constructor(position, width, height, id) {
+        this.position = position,
+        this.height = height,
+        this.width = width,
+        this.id = id
+    }
+
+    draw() {
+        if (this.position.y >= 0 && this.position.y <= floorY) {
+            ctx.fillStyle = "blue";
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        }
+    }
+}
+
+Player.draw();
